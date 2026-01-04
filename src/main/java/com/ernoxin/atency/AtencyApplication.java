988ccd5dafc08dd6e64f72dccd25448b75b99d32
Erfan Davoodi Nasr <davoodinasr.erfan@gmail.com>
2026-01-04 21:39:24 +0330

    init commit

---
+++ src/main/java/com/ernoxin/atency/AtencyApplication.java
@@ -0,0 +1,13 @@
+package com.ernoxin.atency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ernoxin/atency/AtencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernoxin.atency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
